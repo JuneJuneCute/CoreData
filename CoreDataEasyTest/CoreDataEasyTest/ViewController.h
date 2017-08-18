6d234c2fecf8f3cddc838475836a54a5b5c1bb4d
wang <wang@163.com>
2017-08-18 14:37:06 +0800

    初始化项目并添加CoreData数据模型

---
+++ CoreDataEasyTest/CoreDataEasyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataEasyTest
+//
+//  Created by uzone on 2017/8/18.
+//  Copyright © 2017年 uzone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataEasyTest/CoreDataEasyTest/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  CoreDataEasyTest
+//
+//  Created by uzone on 2017/8/18.
+//  Copyright © 2017年 uzone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // 创建一个数据库 company.sqlite
+    //数据库要一张表 员工表 (name,age,height)
+    //往数据库添加员工信息
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
